e2e50a13cd52dc17c4b179ff039582d1b90644e7
Ronny Manuel De la Cruz Perez <62533639+DeLaCruz-Ronny@users.noreply.github.com>
2025-02-01 22:43:58 -0400

    Chat Completo

---
+++ chat_IA/chat_IA/Constantes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chat_IA
+{
+    internal class Constantes
+    {
+        public const string OpenAIKey = "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";
+    }
+}
---
+++ chat_IA/chat_IA/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using chat_IA;
+using OpenAI.Chat;
+using System.Text;
+
+var modelo = "gpt-4o";
+var cliente = new ChatClient(modelo, Constantes.OpenAIKey);
+
+//Se creara un listado de los mensajes que se envien a la IA para que siga el hilo
+//de lo que estamos hablando
+var mensajes = new List<ChatMessage>();
+
+Console.WriteLine("IA: Hola soy el asistente del señor Ronny De La Cruz, como puedo ayudarte?");
+Console.WriteLine();
+
+while (true)
+{
+
+    var sb = new StringBuilder();
+
+    Console.Write("Tu: ");
+    var textIn = Console.ReadLine();
+
+    // Si el usuario no escribe nada salimos
+    if (string.IsNullOrWhiteSpace(textIn))
+    {
+        break;
+    }
+
+    //Agregamos a la lista los mensajes que el usuario le envia a la IA
+    mensajes.Add(new UserChatMessage(textIn));
+
+    Console.WriteLine();
+    Console.Write($"IA: ");
+
+    var stream = cliente.CompleteChatStreamingAsync(mensajes);
+
+    // Hacemos este foreach para que muestre el texto letra por letra como lo hacen las IA habitualemente
+    await foreach (var actualizacion in stream)
+    {
+        foreach (var contenido in actualizacion.ContentUpdate)
+        {
+            sb.Append(contenido.Text);
+            Console.Write(contenido.Text);
+        }
+    }
+
+    mensajes.Add(new AssistantChatMessage(sb.ToString()));
+
+
+    //var IAresponse = await cliente.CompleteChatAsync(mensajes);
+    //var respuestIA = IAresponse.Value.Content[0].Text;
+    //mensajes.Add(new AssistantChatMessage(respuestIA));
+    //Console.WriteLine($"IA: {respuestIA}");
+
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+
+
+
+
